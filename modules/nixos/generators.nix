ebb57292b984e2d2322db4a3c13ce685ea9cad0d
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-21 22:57:20 +0100

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ modules/nixos/generators.nix
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{ inputs, system, ... }:
+let
+  pkgs = import inputs.nixpkgs { inherit system; };
+  inherit (pkgs) lib;
+in
+{
+  imports = [ inputs.nixos-generators.nixosModules.all-formats ];
+
+  formatConfigs.proxmox-lxc = {
+    proxmoxLXC.manageHostName = true;
+  };
+
+  formatConfigs.docker = {
+    networking.firewall.enable = lib.mkForce false;
+    services.fail2ban.enable = lib.mkForce false;
+    services.openssh.startWhenNeeded = lib.mkForce false;
+  };
+
+  nixpkgs.hostPlatform = system;
+}
