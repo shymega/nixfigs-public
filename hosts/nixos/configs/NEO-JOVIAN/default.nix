0ead0425f3ed737f2150137eaf39159b687f9f98
Dom Rodriguez <shymega@shymega.org.uk>
2024-11-17 20:30:14 +0000

    feat: Add NixOS-Jovian configs (again)
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/nixos/configs/NEO-JOVIAN/default.nix
@@ -0,0 +1,187 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+{ inputs
+, config
+, pkgs
+, ...
+}:
+let
+  enableXanmod = true;
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    inputs.jovian-nixos.nixosModules.default
+  ];
+
+  networking = {
+    hostName = "NEO-JOVIAN";
+    hostId = "c45bcf1b";
+  };
+  boot = {
+    supportedFilesystems = [
+      "ntfs"
+      "zfs"
+    ];
+    zfs.extraPools = [ "ztank" ];
+    zfs.devNodes = "/dev/disk/by-partuuid";
+
+    initrd.supportedFilesystems = [
+      "ntfs"
+      "zfs"
+    ];
+
+    initrd = {
+      availableKernelModules = [
+        "nvme"
+        "xhci_pci"
+        "usbhid"
+        "hid_apple"
+      ];
+    };
+
+    kernelParams = [
+      "zfs.zfs_arc_max=12884901888"
+      "nohibernate"
+    ];
+    extraModprobeConfig = ''
+      options zfs l2arc_noprefetch=0 l2arc_write_boost=33554432 l2arc_write_max=16777216 zfs_arc_max=2147483648
+    '';
+
+    kernelPackages =
+      if enableXanmod then
+        pkgs.linuxPackagesFor
+          (
+            pkgs.unstable.linux_xanmod_latest.override {
+              argsOverride = rec {
+                modDirVersion = "${version}-${suffix}";
+                suffix = "xanmod1";
+                version = "6.11.2";
+
+                src = pkgs.fetchFromGitHub {
+                  owner = "xanmod";
+                  repo = "linux";
+                  rev = "${version}-${suffix}";
+                  hash = "sha256-4BXPZs8lp/O/JGWFIO/J1HyOjByaqWQ9O6/jx76TIDs=";
+                };
+              };
+            }
+          )
+      else
+        config.boot.zfs.package.latestCompatibleLinuxPackages;
+
+    extraModulePackages = with config.boot.kernelPackages; [ zfs ];
+
+    kernel.sysctl = {
+      "fs.inotify.max_user_watches" = "819200";
+      "kernel.printk" = "3 3 3 3";
+    };
+
+  };
+
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "powersave";
+  };
+
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        amdvlk
+        # VA-API and VDPAU
+        vaapiVdpau
+
+        # AMD ROCm OpenCL runtime
+        rocmPackages.clr
+        rocmPackages.clr.icd
+      ];
+      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+    };
+    amdgpu = {
+      amdvlk = {
+        enable = true;
+        support32Bit.enable = true;
+      };
+      opencl.enable = true;
+    };
+    i2c.enable = true;
+    cpu.amd.ryzen-smu.enable = true;
+  };
+
+  services = {
+    handheld-daemon = {
+      enable = true;
+      package = pkgs.handheld-daemon;
+      user = "dzrodriguez";
+    };
+    zfs = {
+      trim = {
+        enable = true;
+        interval = "Sat *-*-* 04:00:00";
+      };
+      autoScrub = {
+        enable = true;
+        interval = "Sat *-*-* 05:00:00";
+      };
+      autoSnapshot.enable = true;
+    };
+    xserver = {
+      enable = true;
+      videoDrivers = [ "amdgpu" ];
+    };
+    fstrim.enable = true;
+    smartd = {
+      enable = true;
+      autodetect = true;
+    };
+    power-profiles-daemon.enable = true;
+    input-remapper.enable = true;
+    thermald.enable = true;
+    udev = {
+      packages = with pkgs; [ gnome.gnome-settings-daemon ];
+      extraRules = ''
+        # Workstation - keyboard & mouse
+        ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", ATTR{idProduct}=="024f", ATTR{power/autosuspend}="-1"
+        ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="1bcf", ATTR{idProduct}=="0005", ATTR{power/autosuspend}="-1"
+      '';
+    };
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
+  };
+
+  jovian = {
+    steam = {
+      enable = true;
+      autoStart = true;
+      desktopSession = "plasma";
+      user = "dzrodriguez";
+    };
+    decky-loader = {
+      enable = true;
+      user = "dzrodriguez";
+    };
+    devices.steamdeck = {
+      autoUpdate = false;
+      enable = false;
+      enableGyroDsuService = false;
+    };
+  };
+
+  system.stateVersion = "24.05";
+  services.xserver.desktopManager.plasma5.enable = true;
+}
