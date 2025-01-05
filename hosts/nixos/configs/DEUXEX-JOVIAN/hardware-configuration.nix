bdb379a93f37f4d670f432284fde20d868a0fd90
Dom Rodriguez <shymega@shymega.org.uk>
2025-01-05 21:08:03 +0000

    feat: Add initial DEUSEX-JOVIAN config
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/nixos/configs/DEUXEX-JOVIAN/hardware-configuration.nix
@@ -0,0 +1,14 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  imports = [./disks.nix];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
