b628845207877b87bec29ea10c2efad8c2722bd5
Dom Rodriguez <shymega@shymega.org.uk>
2025-01-05 21:08:03 +0000

    feat: Add initial DEUSEX-JOVIAN config
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/nixos/configs/DEUXEX-JOVIAN/default.nix
@@ -0,0 +1,305 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+{
+  inputs,
+  config,
+  pkgs,
+  ...
+}: let
+  zfsIsUnstable = config.boot.zfs.package == pkgs.zfsUnstable;
+  myCompatibleKernelPackages =
+    lib.filterAttrs (
+      name: kernelPackages:
+        (lib.hasInfix "_xanmod" name)
+        && (builtins.tryEval kernelPackages).success
+        && (
+          (
+            (!zfsIsUnstable && !kernelPackages.zfs.meta.broken)
+            || (zfsIsUnstable && !kernelPackages.zfs_unstable.meta.broken)
+          )
+          && (!kernelPackages.evdi.meta.broken)
+          && (!kernelPackages.vmware.meta.broken)
+        )
+    )
+    pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues myCompatibleKernelPackages
+    )
+  );
+  zfs_arc_max = toString (8 * 1024 * 1024 * 1024);
+  zfs_arc_min = toString (8 * 1024 * 1024 * 1024 - 1);
+in {
+  imports = [
+    ./hardware-configuration.nix
+    inputs.jovian-nixos.nixosModules.default
+    inputs.ucodenix.nixosModules.default
+    inputs.nur-xddxdd.nixosModules.setupOverlay
+    inputs.nur-xddxdd.nixosModules.qemu-user-static-binfmt
+    inputs.nur-xddxdd.nixosModules.nix-cache-attic
+    inputs.nixfigs-virtual-private.virtual.all
+  ];
+
+  lantian.qemu-user-static-binfmt = {
+    enable = true;
+    package = pkgs.qemu;
+  };
+  services.ucodenix = {
+    enable = false; # TODO: Find `cpuModelId` and re-enable.
+    cpuModelId = "00A70F52";
+  };
+
+  networking = {
+    hostName = "DEUSEX-JOVIAN";
+    hostId = "c45bcf1b"; # FIXME: Update with new hostId.
+  };
+  boot = {
+    binfmt = {
+      emulatedSystems = [
+        "wasm32-wasi"
+        "wasm64-wasi"
+      ];
+    };
+    supportedFilesystems = [
+      "ntfs"
+      "zfs"
+    ];
+    initrd = {
+      supportedFilesystems = [
+        "ntfs"
+        "zfs"
+      ];
+
+      luks.devices = {
+        nixos = {
+          device = "/dev/disk/by-label/NIXOS";
+          preLVM = true;
+          allowDiscards = true;
+        };
+      };
+      systemd.services = {
+        rollback = {
+          description = "Rollback ZFS datasets to a pristine state";
+          wantedBy = ["initrd.target"];
+          after = ["zfs-import-ztank.service"];
+          before = ["sysroot.mount"];
+          path = with pkgs; [zfs];
+          unitConfig.DefaultDependencies = "no";
+          serviceConfig.Type = "oneshot";
+          script = ''
+            zfs rollback -r ztank/crypt/nixos-jovian/local/root@blank
+          '';
+        };
+        create-needed-for-boot-dirs = {
+          after = pkgs.lib.mkForce ["zfs-import-ztank.service"];
+          wants = pkgs.lib.mkForce ["zfs-import-ztank.service"];
+        };
+      };
+    };
+    zfs = {
+      extraPools = ["ztank", "zdata"];
+      devNodes = "/dev/disk/by-partuuid";
+    };
+
+    kernelParams = [
+      "nohibernate"
+      "zfs.zfs_arc_max=${zfs_arc_max}"
+      "zfs.zfs_arc_min=${zfs_arc_min}"
+      "zfs.l2arc_write_boost=33554432"
+      "zfs.l2arc_write_max=16777216"
+    ];
+    extraModprobeConfig = ''
+      options kvm_amd nested=1
+      options kvm ignore_msrs=1 report_ignored_msrs=0
+    '';
+
+    kernelPackages = latestKernelPackage;
+
+    extraModulePackages = with config.boot.kernelPackages; [zfs evdi vmware];
+
+    kernel.sysctl = {
+      "fs.inotify.max_user_watches" = "819200";
+      "kernel.printk" = "3 3 3 3";
+    };
+
+    plymouth = {
+      enable = true;
+    };
+
+    lanzaboote = {
+      enable = false; # TODO: Reenable once `extraEntries` available upstream.
+      enrollKeys = false;
+      configurationLimit = 3;
+      pkiBundle = "/etc/secureboot";
+    };
+    loader = {
+      systemd-boot = {
+        enable = true;
+        memtest86.enable = true;
+        netbootxyz.enable = true;
+        extraFiles = {
+          "efi/shell/shellx64.efi" = "${pkgs.edk2-uefi-shell}/shell.efi";
+        };
+        extraEntries = {
+          "shell.conf" = ''
+            title UEFI shell
+            efi /EFI/SHELL/SHELLX64.EFI
+          '';
+        };
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+      generationsDir.copyKernels = true;
+      timeout = 6;
+    };
+  };
+
+  powerManagement = {
+    enable = true;
+    cpuFreqGovernor = "powersave";
+  };
+
+  hardware = {
+    gpd.ppt.enable = lib.mkForce false;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        amdvlk
+        # VA-API and VDPAU
+        vaapiVdpau
+
+        # AMD ROCm OpenCL runtime
+        rocmPackages.clr
+        rocmPackages.clr.icd
+      ];
+      extraPackages32 = with pkgs; [driversi686Linux.amdvlk];
+    };
+    amdgpu = {
+      initrd.enable = true;
+      amdvlk = {
+        enable = true;
+        support32Bit.enable = true;
+      };
+      opencl.enable = true;
+    };
+    i2c.enable = true;
+    sensor.iio = {
+      enable = true;
+    };
+    cpu.amd.ryzen-smu.enable = true;
+  };
+
+  services = {
+    power-profiles-daemon.enable = pkgs.lib.mkForce false;
+    fwupd.enable = true;
+    hardware.bolt.enable = true;
+    handheld-daemon = {
+      enable = true;
+      package = pkgs.handheld-daemon;
+      user = "dzrodriguez";
+    };
+    zfs = {
+      trim = {
+        enable = true;
+        interval = "Sat *-*-* 05:00:00";
+      };
+      autoScrub = {
+        enable = true;
+        interval = "Sat *-*-* 07:00:00";
+      };
+      autoSnapshot.enable = true;
+    };
+    xserver = {
+      enable = true;
+      videoDrivers = ["amdgpu"];
+    };
+    fstrim.enable = true;
+    smartd = {
+      enable = true;
+      autodetect = true;
+    };
+    thermald.enable = true;
+    udev = {
+      packages = with pkgs; [gnome-settings-daemon];
+      extrarules = ''
+        SUBSYSTEM=="power_supply", KERNEL=="adp1", ATTR{online}=="0", RUN+="${pkgs.lib.getexe' pkgs.systemd "systemctl"} --no-block start battery.target"
+        SUBSYSTEM=="power_supply", KERNEL=="adp1", ATTR{online}=="1", RUN+="${pkgs.lib.getexe' pkgs.systemd "systemctl"} --no-block start ac.target"
+
+        # workstation - keyboard & mouse suspension.
+        ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="05ac", ATTR{idProduct}=="024f", ATTR{power/autosuspend}="-1"
+        ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="1bcf", ATTR{idProduct}=="0005", ATTR{power/autosuspend}="-1"
+
+        # 4g lte modem.
+        ACTION=="add|change", SUBSYSTEM=="usb", ATTR{idVendor}=="2c7c", ATTR{idProduct}=="0125", ATTR{power/autosuspend}="-1"
+
+        # workstation - thinkpad dock (40ac).
+        SUBSYSTEM=="usb", ACTION=="add|change", ATTR{idVendor}=="17ef", ATTR{idProduct}=="3066", SYMLINK+="docked", SYMLINK+="docked", TAG+="systemd"
+
+        # kvm input - active.
+        SUBSYSTEM=="usb", ACTION=="add|change|remove", ATTR{idVendor}=="13ba", ATTR{idProduct}=="0018",  SYMLINK+="currkvm", TAG+="systemd"
+
+        # rename network interface.
+        SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{DEVTYPE}=="wlan", KERNEL=="wlan*", name="wlan0"
+
+        # my personal iphone.
+        SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{ID_MODEL_ID}=="12a8", KERNEL=="eth*", name="iphone0"
+
+        # my personal op6t.
+        SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{ID_MODEL_ID}=="9024", KERNEL=="usb*", name="android0"
+
+        # my personal moto g.
+        SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{ID_MODEL_ID}=="201c", KERNEL=="usb*", name="android0"
+
+        # thinkpad docking station ethernet.
+        SUBSYSTEM=="net", ACTION=="add|change", DRIVERS=="?*", ENV{ID_MODEL_ID}=="3069", KERNEL=="eth*", name="docketh0"
+
+        # wm2 i2c fixes.
+        SUBSYSTEM=="i2c", KERNEL=="i2c-gxtp7385:00", ATTR{power/wakeup}="disabled"
+      '';
+    };
+    ollama = {
+      enable = false; # FIXME: Reenable when 890M/NPU support is added.
+      package = pkgs.unnstable.ollama;
+      acceleration = "rocm";
+      models = "/data/AI/LLMs/Ollama/Models/";
+    };
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+          governor = "powersave";
+          turbo = "never";
+        };
+        charger = {
+          governor = "performance";
+          turbo = "auto";
+        };
+      };
+    };
+    xserver.desktopManager.plasma5.enable = true;
+  };
+
+  jovian = {
+    steam = {
+      enable = true;
+      autoStart = true;
+      desktopSession = "plasma";
+      user = "dzrodriguez";
+    };
+    decky-loader = {
+      enable = true;
+      user = "dzrodriguez";
+    };
+    devices.steamdeck = {
+      autoUpdate = false;
+      enable = false;
+      enableGyroDsuService = false;
+    };
+  };
+
+  system.stateVersion = "24.11";
+}
