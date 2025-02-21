5b0c53b1dbf633bfbe28929412b439d76da4b046
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:47 +0000

    feat: Add DEUSEX-LINUX
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ hosts/nixos/configs/DEUSEX-LINUX/hardware-configuration.nix
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+{
+  config,
+  lib,
+  ...
+}: {
+  imports = [./disks.nix];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
